d990e4850bc5771e8225ff58e7a8887566bbc9c8
Brecht Cloetens <brecht@codeurs.be>
2026-09-06 12:02:03 +0200

    fix(data): Add normal variants of swsh7/16 (#2329)
    * fix(data): Add normal variants of swsh7/16
    
    * fix(data): Update cardmarket id's for swsh7/16

--- data/Sword & Shield/Evolving Skies/16.ts
+++ data/Sword & Shield/Evolving Skies/16.ts
@@ -106,6 +106,21 @@ const card: Card = {
 				tcgplayer: 246831
 			}
 		},
+		{
+			type: 'normal',
+			thirdParty: {
+				cardmarket: 701491,
+				tcgplayer: 247422
+			}
+		},
+		{
+			type: 'normal',
+			stamp: ['player-rewards-program'],
+			thirdParty: {
+				cardmarket: 697069,
+				tcgplayer: 475978
+			}
+		},
 	],
 }
 
